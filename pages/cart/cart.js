e8182dba628f48d1f9876b44bf675f400e886300
BarryDong <donghao@bwcj.com>
2024-10-04 22:04:03 +0800

    购物车-静态页面

--- pages/cart/cart.js
+++ pages/cart/cart.js
@@ -1,6 +1,8 @@
-Page({
-  data: {},
-  onLoad: function (options) {
-
-  }
+Component({
+  properties: {},
+  data: {
+    cartList: [1, 2, 3, 4],
+    emptyDes: '还没有添加商品，快去添加吧～'
+  },
+  methods: {}
 })
