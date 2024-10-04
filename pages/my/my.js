04818ee19c48a0663e64004a9b94fc8b544ea98a
BarryDong <donghao@bwcj.com>
2024-10-04 22:10:24 +0800

    我的-静态页面

--- pages/my/my.js
+++ pages/my/my.js
@@ -1,6 +1,26 @@
 Page({
-  data: {},
-  onLoad: function (options) {
-
+  data: {
+    initpanel: [
+      {
+        url: '/pages/order/list/list',
+        title: '商品订单',
+        iconfont: 'icon-dingdan'
+      },
+      {
+        url: '/pages/order/list/list',
+        title: '礼品卡订单',
+        iconfont: 'icon-lipinka'
+      },
+      {
+        url: '/pages/order/list/list',
+        title: '退款/售后',
+        iconfont: 'icon-tuikuan'
+      }
+    ]
+  },
+  toLoginPage() {
+    wx.navigateTo({
+      url: '/pages/login/login'
+    })
   }
 })
